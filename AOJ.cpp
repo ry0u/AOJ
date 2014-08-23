bfcc73eb2e1979f086d6b524820e1aa48f5c363a
ry0u <ry0u_yd@yahoo.co.jp>
2014-08-23 23:36:36 +0900

    test commit

---
+++ AOJ.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <string.h>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <complex>
+#include <cstdlib>
+#include <map>
+
+#define REP(i,k,n) for(int i=k;i<n;i++)
+#define rep(i,n) for(int i=0;i<n;i++)
+#define pb push_back
+#define mp make_pair
+
+using namespace std;
+
+int main()
+{
+	return 0;
+}
